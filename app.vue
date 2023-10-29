ab499646049670284718a9aea5ee592c8dbb36f6
Alex Koledaev <ax3l3rator@gmail.com>
2023-10-29 23:40:16 +0300

    ⚡ Override webkit-scrollbar

--- app.vue
+++ app.vue
@@ -5,3 +5,21 @@
     </v-app>
   </NuxtLayout>
 </template>
+<style>
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 6px;
+  background: rgba(var(--v-theme-surface));
+  border: solid 3.8px transparent;
+  background-clip: content-box;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: rgba(var(--v-theme-surface-variant));
+  border: solid 3px transparent;
+  background-clip: content-box;
+}
+</style>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,8 +1,23 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+//@ts-expect-error
+import colors from 'vuetify/lib/util/colors';
 
 export default defineNuxtPlugin((app) => {
-  const vuetify = createVuetify({});
+  const vuetify = createVuetify({
+    theme: {
+      defaultTheme: 'dark',
+      themes: {
+        dark: {
+          dark: true,
+          colors: {
+            primary: colors.blue.base,
+            'primary-lighten-1': colors.blue.lighten1,
+          },
+        },
+      },
+    },
+  });
   app.vueApp.use(vuetify);
 });
